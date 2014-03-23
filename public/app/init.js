5838b43837a5631715f7849d98f2ae566f6b0039
shaulm <shaul.mazor@gmail.com>
2014-03-23 17:08:16 +0200

    fix sub folders names issues

--- public/app/init.js
+++ public/app/init.js
@@ -2,9 +2,9 @@
  this is the entry point to the application
  we will load the require configuration and then the application logic
  */
-require(["app/starters/requirejsConfigs"], function (parsedConfig) {
+require(["app/starters/requirejsConfigs"], function (fullConfig) {
 
-    requirejs.config(parsedConfig);
+    requirejs.config(fullConfig);
 
     require(["mbApp"], function (app) {
 
