c9100de32e5a5afc3ba2d0d68add0742532c9cce
shaulm <shaul.mazor@gmail.com>
2014-04-24 17:29:02 +0300

    great job

--- public/app/frame/ui/i18n/en-US.js
+++ public/app/frame/ui/i18n/en-US.js
@@ -3,7 +3,7 @@ define(function () {
 
     var Dictionary = {
 
-        'mb.title': 'The Art of MVC in a Real-World App!',
+        'mb.title': 'The Art of MVC in a Gmail-Like Application!',
         'mb.techbar.about' : 'About',
         'mb.techbar.tutorial' : 'Tutorial',
         'mb.techbar.resources' : 'Resources'
