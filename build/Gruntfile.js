27dd83c3a1ab376a4ae2589147a49854a79eb038
shaul.mazor <shaul.mazor@gmail.com>
2014-10-02 20:10:06 +0300

    bug

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,16 +1,5 @@
 module.exports = function(grunt) {
 
-    grunt.registerTask('prepareProd', 'bla', function (src, dest) {
-
-        var fs = require('fs-extra');
-
-        if(grunt.file.exists('d:/smd/mailbone-prod1')){
-            grunt.file.delete('d:/smd/mailbone-prod1',{force: true});
-        }
-
-        grunt.file.mkdir('d:/smd/mailbone-prod1');
-    });
-
     grunt.initConfig({
 
         pkg:grunt.file.readJSON('package.json'),
@@ -99,19 +88,8 @@ module.exports = function(grunt) {
         copy: {
             targetFiles: {
                 files: [
-                    {expand: true, cwd: '../public/target/', src: ['**'],dest: 'd:/smd/mailbone-prod1/'}
+                    {expand: true, cwd: '../public/target/', src: ['**'],dest: '../../mailbone-prod/public'}
                 ]
-            }
-        },
-
-        exec: {
-            task1: {
-                cmd: function(firstName, lastName) {
-                    return "IF Exist 'd:/smd/mailbone-prod1' (rd 'd:/smd/mailbone-prod1' /S /Q);";
-                }
-            },
-            task2: {
-                cmd: "xcopy  public' 'mailbone-prod1'"
             }
         }
     });
@@ -136,46 +114,6 @@ module.exports = function(grunt) {
         'jshint',
         'requirejs',
         'clean:output',
-        //'prepareProd',
-        //'copy:targetFiles'
-        //'exec:task1',
-        'exec:task2'
+        'copy:targetFiles'
     ]);
 };
-
-
-
-//grunt.registerTask('install', 'install the backend and frontend dependencies', function() {
-//    var async = require('async');
-//    var exec = require('child_process').exec;
-//    var done = this.async();
-//
-//    var runCmd = function(item, callback) {
-//        process.stdout.write('running "' + item + '"...\n');
-//        var cmd = exec(item);
-//        cmd.stdout.on('data', function (data) {
-//            grunt.log.writeln(data);
-//        });
-//        cmd.stderr.on('data', function (data) {
-//            grunt.log.errorlns(data);
-//        });
-//        cmd.on('exit', function (code) {
-//            if (code !== 0) throw new Error(item + ' failed');
-//            grunt.log.writeln('done\n');
-//            callback();
-//        });
-//    };
-//
-//    async.series({
-//            npm: function(callback){
-//                runCmd('npm install', callback);
-//            },
-//            bower: function(callback){
-//                runCmd('bower install', callback);
-//            }
-//        },
-//        function(err, results) {
-//            if (err) done(false);
-//            done();
-//        });
-//});
