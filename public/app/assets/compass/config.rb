f1dc5e5dcb618e1044086af4d33968c8d5b40b9d
shaulm <Haa!1234>
2014-10-05 17:03:02 +0300

    structure  fixes

--- public/app/assets/compass/config.rb
+++ public/app/assets/compass/config.rb
@@ -2,20 +2,21 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../app/assets/ui/css"
-sass_dir = "../../app/assets/ui/scss"
-images_dir = "../../app/assets/ui/img"
-javascripts_dir = "../../app/assets/js"
+css_dir = "../ui/css"
+sass_dir = "../ui/scss"
+images_dir = "../ui/img"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
- relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
  line_comments = false
 
+
+http_images_path = "../app/assets/ui/img"
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
@@ -23,11 +24,11 @@ javascripts_dir = "../../app/assets/js"
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
-additional_import_paths = ["../../app/modules/mail/ui/scss",
-                           "../../app/modules/tasks/ui/scss",
-                           "../../app/core/frame/ui/scss",
-                           "../../app/assets/ui/components/autoComplete/ui/scss",
-                           "../../app/assets/ui/components/tags/ui/scss",
-                           "../../app/assets/ui/components/dialog/ui/scss",
-                           "../../app/assets/ui/components/search/ui/scss",
-                           "../../app/assets/ui/scss"]
+additional_import_paths = ["../../modules/mail/ui/scss",
+                           "../../modules/tasks/ui/scss",
+                           "../../core/frame/ui/scss",
+                           "../ui/components/autoComplete/ui/scss",
+                           "../ui/components/tags/ui/scss",
+                           "../ui/components/dialog/ui/scss",
+                           "../ui/components/search/ui/scss",
+                           "../ui/scss"]
