b38e7354b9065141dbed4c1171c9370fd758afb5
shaulm <shaulm@liveperson.com>
2015-04-28 08:14:47 +0300

    set favicon and logo

---
+++ backEnd/appStart.js
@@ -0,0 +1,50 @@
+module.exports = function() {
+
+    var mails = require('./routes/mails/mails');
+    var dbManager = require('./managers/dbManager');
+    var socketManager = require('./managers/socketManager');
+
+    var start = function(server){
+
+        var io = require('socket.io').listen(server);
+
+        dbManager.connect(function(Models) {
+
+            mails.setModel(Models.MailModel);
+
+            io.sockets.on('connection', function (socket) {
+
+                socketManager.setIO(io);
+
+                socket.on('add-user', function (userName) {
+                    socketManager.addSocket(socket, userName);
+                });
+                socket.on('disconnect', function () {
+                    socketManager.removeSocket(socket);
+                });
+                socket.on('mail:create', function (userName, data) {
+                    mails.addItem(socket, userName, data);
+                });
+                socket.on('mail:delete', function (userName, data) {
+                    mails.deleteItem(socket, userName, data);
+                });
+                socket.on('mails:read', function (userName, data) {
+                    mails.getList(socket, userName, data);
+                });
+                socket.on('mails:delete', function (userName, data) {
+                    mails.deleteBulk(socket, userName, data);
+                });
+                socket.on('mail:update', function (userName, data) {
+                    mails.updateItem(socket, userName, data);
+                });
+                socket.on('mails:update', function (userName, data) {
+                    mails.updateBulk(socket, userName, data);
+                });
+            });
+        });
+    };
+
+    return{
+        start:start
+    }
+}();
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 var PORT = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8000;
 var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-console.log("PORT " + PORT);
-console.log("IPADDRESS " + IPADDRESS);
-
 var path = require('path');
 var express = require('express');
-var mails = require('./backEnd/routes/mails/mails');
-var dbManager = require('./backEnd/managers/dbManager');
-var socketManager = require('./backEnd/managers/socketManager');
+var appStart = require('./backEnd/appStart');
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'frontEnd')));
@@ -16,42 +11,4 @@ app.use(express.static(path.join(__dirname, 'frontEnd')));
 var server = require('http').createServer(app);
 server.listen(PORT, IPADDRESS);
 
-var io = require('socket.io').listen(server);
-
-dbManager.connect(function(Models){
-
-    mails.setModel(Models.MailModel);
-
-    io.sockets.on('connection', function (socket) {
-
-        console.log("io.sockets connection successful");
-        socketManager.setIO(io);
-
-        socket.on('add-user', function(userName){
-            console.log("add-user successful");
-            socketManager.addSocket(socket, userName);
-        });
-        socket.on('mail:create', function (userName, data) {
-            console.log("mail:create successful");
-            mails.addItem(socket, userName, data);
-        });
-        socket.on('mail:delete', function (userName, data) {
-            mails.deleteItem(socket, userName, data);
-        });
-        socket.on('mails:read', function (userName, data) {
-            mails.getList(socket, userName, data);
-        });
-        socket.on('mails:delete', function (userName, data) {
-            mails.deleteBulk(socket, userName, data);
-        });
-        socket.on('mail:update', function (userName, data) {
-            mails.updateItem(socket, userName, data);
-        });
-        socket.on('mails:update', function (userName, data) {
-            mails.updateBulk(socket, userName, data);
-        });
-        socket.on('disconnect', function() {
-            socketManager.removeSocket(socket);
-        });
-    });
-});
+appStart.start(server);
